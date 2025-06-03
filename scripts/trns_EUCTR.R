9dafe8dc66e00b93ee4af005cda82eeb79533b96
ontogenerator <vladislav.nachev@charite.de>
2025-06-03 16:07:51 +0200

    finish secondary TRN in EUCTR

--- scripts/trns_EUCTR.R
+++ scripts/trns_EUCTR.R
@@ -7,6 +7,7 @@ library(here)
 euctr_combined <- readRDS(here("data", "raw", "euctr_combined.rds"))
 source(here("scripts", "utils.R"))
 
+regexes <- get_registry_regex(c("DRKS", "ClinicalTrials.gov", "EudraCT"))
 
 euctr_trns <- euctr_combined |> 
   select(contains("eudract_number"), contains("isrctn"),
@@ -22,8 +23,6 @@ plan(multisession)
 #plan(sequential)
 handlers(global = TRUE)
 
-# columns_to_clean <- c("results_isrctn_number", "results_nct_number", "results_other_ids")
-# invalid_ncts <- c("NCT00000000", "NCT99999999", "NCT12345678")
 na_regexes <- c("\\b[Nn]\\W?[Aa]\\b",
                 "00000000",
                 "99999999",
@@ -35,107 +34,68 @@ na_regexes <- c("\\b[Nn]\\W?[Aa]\\b",
 #               "ISRCTN00000000", "-", "Name: - Number: -")
 # str_view(examples, na_regexes)
 
+extract_valid <- function(trn_vec, valid_regex, na_regex, clean = FALSE) {
+  dplyr::case_when(
+    stringr::str_detect(trn_vec, na_regex) ~ NA_character_,
+    clean == TRUE & stringr::str_detect(trn_vec, valid_regex) ~ 
+      stringr::str_extract(trn_vec, valid_regex) |> ctregistries::which_trns(),
+    .default = stringr::str_extract(trn_vec, valid_regex)
+  )
+}
+
 euctr_trns_clean <- euctr_trns |> 
   # select(all_of(columns_to_clean), everything()) |> 
   mutate(across(everything(), \(x) if_else(str_detect(x, na_regexes), NA_character_, x))) |> 
   filter(!if_all(-contains("eudract"), is.na))  |> # Remove fully NA rows (if any)
-  
-  
-mutate(ctgov_clean = ifelse(nct_number %in% invalid_ncts, NA_character_, nct_number), 
-         # remove potential self-references here
-         other_clean = other_ids |> str_remove(eudract_number) |> which_trns()) |> 
+  mutate(ctgov_clean  = extract_valid(nct_number, regexes$ClinicalTrials.gov, na_regexes),
+         ctgov_clean_results = extract_valid(results_nct_number, regexes$ClinicalTrials.gov, na_regexes),
+         other_clean = which_trns(other_ids),
+         other_clean_results = which_trns(results_other_ids)
+         )
+
+euctr_trns_clean_registries <- euctr_trns_clean |> 
+  mutate(ctgov_clean_other = extract_valid(other_clean, regexes$ClinicalTrials.gov, na_regexes),
+         ctgov_clean_other_results = extract_valid(other_clean_results, regexes$ClinicalTrials.gov, na_regexes),
+         drks_clean = extract_valid(other_clean, regexes$DRKS, na_regexes),
+         drks_clean_results = extract_valid(other_clean_results, regexes$DRKS, na_regexes),
+         euctr_clean = extract_valid(other_clean, regexes$EudraCT, na_regexes),
+         euctr_clean_results = extract_valid(other_clean_results, regexes$EudraCT, na_regexes))
+
+euctr_trns_clean_all <- euctr_trns_clean_registries |> 
+  mutate(euctr_clean = na_if(euctr_clean, eudract_number),
+         euctr_clean_results = na_if(euctr_clean_results, eudract_number)) |> 
   rowwise() |> 
-  mutate(ctgov_other = str_extract_all(other_clean, regexes$ClinicalTrials.gov) |> 
-           unlist() |> 
-           paste(collapse = ";") |> 
-           na_if("NA") |> 
-           na_if(""),
-         ctgov_all = deduplicate_collapsed(c(ctgov_clean, ctgov_other)),
+  mutate(ctgov_all = deduplicate_collapsed(c(ctgov_clean, ctgov_clean_results, ctgov_clean_other, ctgov_clean_other_results)),
+         drks_all = deduplicate_collapsed(c(drks_clean, drks_clean_results)),
+         euctr_all = deduplicate_collapsed(c(euctr_clean, euctr_clean_results)),
          n_ctgov = str_count(ctgov_all, ";") + 1 * as.numeric(ctgov_all != ""),
-         drks_clean = str_extract_all(other_clean, regexes$DRKS) |> 
-           unlist() |> 
-           paste(collapse = ";") |> 
-           na_if("NA") |> 
-           na_if(""),
-         n_drks = str_count(drks_clean, ";") + 1 * as.numeric(drks_clean != ""),
-         euctr_clean = str_extract_all(other_clean, regexes$EudraCT)|> 
-           unlist() |> 
-           paste(collapse = ";") |> 
-           na_if("NA") |> 
-           na_if(""),
-         n_euctr = str_count(euctr_clean, ";") + 1 * as.numeric(euctr_clean != "")) |> 
+         n_drks = str_count(drks_all, ";") + 1 * as.numeric(drks_clean != ""),
+         n_euctr = str_count(euctr_all, ";") + 1 * as.numeric(euctr_clean != "")) |> 
   ungroup()
 
-qa_results_clean <- euctr_results_trns_clean |> 
-  filter(str_detect(other_ids, eudract_number)) |> 
-  select(eudract_number, other_ids, other_clean, euctr_clean, everything())
 
-euctr_trns <- euctr_tib |> 
-  select(eudract_number,
-         protocol_sponsor_code = sponsor_s_protocol_code_number,
-         isrctn_number = isrctn_international_standard_randomised_controlled_trial_numbe,
-         nct_number = us_nct_clinicaltrials_gov_registry_number,
-         who_utn_number = who_universal_trial_reference_number_utrn,
-         other_ids = other_identifiers) 
+qa_results_clean <- euctr_trns_clean_all |> 
+  filter(str_detect(results_other_ids, eudract_number)) |> 
+  select(eudract_number, results_other_ids, other_clean_results, euctr_all, everything())
 
 # is protocol_sponsor_code unique for countries? does it matter if collapsed to 1?
 
-euctr_protocols <- euctr_trns |> 
-  # select(-protocol_sponsor_code) |> 
-  # janitor::get_dupes(eudract_number) |>
-  # filter(eudract_number %in% dupe_tests$eudract_number) |> 
-  mutate(across(everything(),
-                \(x) if_else(str_detect(x, "0000\\W?0000|1234-1234|12345678"),
-                             NA_character_, as.character(x)))) |> 
-  filter(str_detect(nct_number, regexes$ClinicalTrials.gov) |
-           str_detect(other_ids, paste(c(regexes$ClinicalTrials.gov,
-                                         regexes$DRKS,
-                                         regexes$EudraCT), collapse = "|"))) |> 
-  distinct(pick(everything()))
-
-euctr_protocols_trns_clean <- euctr_protocols |> 
-  mutate(ctgov_clean = ifelse(nct_number %in% invalid_ncts, NA_character_, nct_number),
-         other_clean = other_ids |> str_remove(eudract_number) |> which_trns()) |> 
-  rowwise() |> 
-  mutate(ctgov_other = str_extract_all(other_clean, regexes$ClinicalTrials.gov) |> 
-           unlist() |> 
-           paste(collapse = ";") |> 
-           na_if("NA") |> 
-           na_if(""),
-         ctgov_all = deduplicate_collapsed(c(ctgov_clean, ctgov_other)),
-         n_ctgov = str_count(ctgov_all, ";") + 1 * as.numeric(ctgov_all != ""),
-         drks_clean = str_extract_all(other_clean, regexes$DRKS) |> 
-           unlist() |> 
-           paste(collapse = ";") |> 
-           na_if("NA") |> 
-           na_if(""),
-         n_drks = str_count(drks_clean, ";") + 1 * as.numeric(drks_clean != ""),
-         euctr_clean = str_extract_all(other_clean, regexes$EudraCT)|> 
-           unlist() |> 
-           paste(collapse = ";") |> 
-           na_if("NA") |> 
-           na_if(""),
-         n_euctr = str_count(euctr_clean, ";") + 1 * as.numeric(euctr_clean != "")) |> 
-  ungroup()
-
-# deduplicate and filter out NAs
-
-euctr_protocols_trns_clean <- euctr_protocols_trns_clean |> 
-  group_by(eudract_number) |> 
-  summarise(across(c(ctgov_all, drks_clean, euctr_clean), deduplicate_collapsed)) |> 
-  # remove self-references from euctr_clean
-  mutate(euctr_clean = ifelse(euctr_clean == eudract_number, NA_character_, euctr_clean)) |> 
-  filter(!is.na(ctgov_all) |
-           !is.na(drks_clean) |
-           !is.na(euctr_clean)) |> 
-  ungroup()
+qa_trns <- euctr_trns_clean_all |>  
+  filter(!is.na(ctgov_all) | !is.na(drks_all) | !is.na(euctr_all)) |> 
+  pull(eudract_number)
 
 # check if stragglers were still missed:
-# all NCTs are non-resolving and malformed
+# all are self-references, so no
 qa_protocols <- euctr_trns |> 
-  filter(!eudract_number %in% euctr_protocols_trns_clean$eudract_number,
-         !nct_number %in% invalid_ncts,
+  filter(!eudract_number %in% qa_trns,
          str_detect(other_ids, paste(c(regexes$ClinicalTrials.gov,
                                        regexes$DRKS,
                                        regexes$EudraCT), collapse = "|")),
          !str_detect(other_ids, eudract_number))
+
+euctr_trns_export <- euctr_trns_clean_all |> 
+  select(contains("eudract_number"), contains("_all"), n_ctgov, n_drks, n_euctr) |> 
+  mutate(across(contains("n_"), \(x) replace_na(x, 0)))
+
+euctr_trns_export |> 
+  write_excel_csv(here("data", "processed", "euctr_ids.csv"))
