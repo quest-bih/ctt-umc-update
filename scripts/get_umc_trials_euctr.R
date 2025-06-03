5655917410669e58da3e33c25853f857951aa674
ontogenerator <vladislav.nachev@charite.de>
2025-06-03 11:59:20 +0200

    add sanity check

--- scripts/get_umc_trials_euctr.R
+++ scripts/get_umc_trials_euctr.R
@@ -29,7 +29,17 @@ trials_tidy <- tibble(id = map_chr(all_trials, pluck("trial_id")),
 # get title and id for EUCTR registered trial with German sponsor
 euctr_trials_german_umc <- trials_tidy |>
   filter(sponsor %in% DE_UMCs$sponsor) |>
-  left_join(DE_UMCs, by = "sponsor") |>
+  left_join(DE_UMCs, by = "sponsor", relationship = "many-to-many") |>
   mutate(title_len = str_length(title)) # title cuts off at 200 and an ellipsis is added if title too long
 
 #write_csv(euctr_trials_german_umc, here("data", "processed", "umc_trials_euctr.csv"))
+
+
+###### sanity checks
+
+# every UMC present, but also Augsburg and also very few trials from Rostock, Witten-Herdecke, Greifswald
+
+umcs_detected <- euctr_trials_german_umc |> 
+  count(UMC) |> 
+  full_join(DE_UMCs, by = "UMC") |> 
+  arrange(n)
